6201d0eee7c1dfe6ec6e6682bc94bca9c5b2b896
Victor Diaconu <58246037+victordi@users.noreply.github.com>
2022-06-05 13:23:50 +0300

    Play/pass (#1)
    * Add gitignore
    
    * Refactor GameState
    
    * Pass or Play View
    
    * Fix scoring system
    
    * Continue buttons + reveal answers
    
    Co-authored-by: Victor <victor.diaconu@ing.com>

--- FamilyFeud/Game.swift
+++ FamilyFeud/Game.swift
@@ -2,96 +2,70 @@ import SwiftUI
 import Foundation
 
 struct GameView: View {
-    var player1: Bool
-    @State var pointsP1: Int
-    @State var pointsP2: Int
-    var teamName1: String
-    var teamName2: String
+    @State var gameState: GameState
     
     @State private var mainScreen = false
-    @State private var nextRound = false
-    @State private var round: Round = games.randomElement().unsafelyUnwrapped
     @State private var answer = ""
-    @State private var strikes = 0
-    
-    func addPoints(_ points: Int) -> Void {
-        if(player1) { pointsP1 = pointsP1 + points }
-        else { pointsP2 = pointsP2 + points }
-    }
-    
+    @State private var next = false
+        
     var body: some View {
         if (mainScreen) {
             ContentView()
         }
+        else if (next) {
+            PassOrPlayView(gameState: gameState.nextRound())
+        }
         else {
-            if (nextRound) {
-                GameView(player1: !player1, pointsP1: pointsP1, pointsP2: pointsP2, teamName1: teamName1, teamName2: teamName2)
+            if (gameState.round.isFinished() || gameState.round.strikes == 3) {
+                // TODO(): if 3 strikes -> allow opponents to steal
+                VStack {
+                    Spacer()
+                    Text("Congratulations \(gameState.currentTeam)")
+                    Text("You won \(gameState.currentPoints) points for this round")
+                    Spacer()
+                    gameState.body
+                    Spacer()
+                    gameState.scoreTable
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button("Reveal all answers") {
+                            gameState.round.reveal()
+                        }
+                        Spacer()
+                        Button("Next round") {
+                            next = true
+                        }
+                        Spacer()
+                    }
+                    Spacer()
+                }
             } else {
                 VStack {
-                    if (player1) { Text(teamName1) }
-                    else { Text(teamName2) }
-                    VStack {
-                        Spacer()
-                        Text(round.question)
-                        Spacer()
-                      
-                        List(round.answers.indices, id: \.self) { i in
-                            HStack {
-                                Text(round.answers[i].getText())
-                                Spacer()
-                                Text(round.answers[i].getPoints())
-                            }
-                        }
-                        
-                        Spacer()
-                        
+                    Text(gameState.currentTeam)
+                    
+                    gameState.body
+                    HStack {
                         TextField("Your answer...", text: $answer)
-                        
                         Spacer()
                         Button("Submit answer") {
                             if (!answer.isEmpty) {
-                                let points = round.tryAnswer(answer)
-                                if (points < 0) {
-                                    strikes = strikes + 1
-                                    if(strikes == 3) { nextRound = true }
+                                let points = gameState.round.tryAnswer(answer)
+                                if (points == 0) {
+                                    gameState.round.strikes += 1
                                 } else {
-                                    addPoints(points)
+                                    gameState.currentPoints += points
                                 }
                             }
+                            answer = ""
                         }
                     }
                     Spacer()
-                    HStack {
-                        Spacer()
-                        VStack {
-                            Text(teamName1)
-                            Text("\(pointsP1)")
-                        }
-                        Spacer()
-                        if (player1) { Text("<---") }
-                        else { Spacer() }
-                        Text("Strikes: \(strikes)")
-                        if (!player1) { Text("--->") }
-                        else { Spacer() }
-                        Spacer()
-                        VStack {
-                            Text(teamName2)
-                            Text("\(pointsP2)")
-                        }
-                        Spacer()
-                    }
+                    gameState.scoreTable
+                    Text("Strikes: \(gameState.round.strikes)")
                     Spacer()
-                    HStack {
-                        Spacer()
-                        Button("Next round") {
-                            nextRound = true
-                        }
-                        Spacer()
-                        Spacer()
-                        Button("Exit game") {
-                            mainScreen = true
-                        }
-                        Spacer()
+                    Button("Exit game") {
+                        mainScreen = true
                     }
                     Spacer()
                 }
@@ -102,96 +76,6 @@ struct GameView: View {
 
 struct GameView_Previews: PreviewProvider {
     static var previews: some View {
-        GameView(player1:true, pointsP1:0, pointsP2:0, teamName1: "Victor", teamName2: "Test")
+        GameView(gameState: GameState(player1:true, pointsP1:0, pointsP2:0, teamName1: "Victor", teamName2: "Test"))
     }
 }
-
-struct Round {
-    var question: String
-    var answers: [Answer]
-    
-    mutating func tryAnswer(_ ans: String) -> Int {
-        for i in 0...answers.count - 1 {
-            if (answers[i].isCorrectAnswer(ans)) {
-                answers[i].isGuessed = true
-                return answers[i].points
-            }
-        }
-        return -1
-    }
-    
-    func isFinished() -> Bool {
-        for i in 0...answers.count - 1 {
-            if (!answers[i].isGuessed) {
-                return false
-            }
-        }
-        return true
-    }
-}
-
-struct Answer {
-    var text: String
-    var points: Int
-    var isGuessed: Bool = false
-    
-    func isCorrectAnswer(_ ans: String) -> Bool {
-        ans.lowercased() == text.lowercased()
-    }
-    
-    func getText() -> String {
-        if (isGuessed) {
-            return text
-            
-        } else {
-            return ""
-        }
-    }
-    
-    func getPoints() -> String {
-        if (isGuessed) {
-            return "\(points)"
-            
-        } else {
-            return ""
-        }
-    }
-}
-
-var game1 = Round(
-    question: "What is your favourite animal?",
-     answers: [
-        Answer(text: "Dog", points: 40),
-        Answer(text: "Cat", points: 30),
-        Answer(text: "Turtle", points: 10),
-        Answer(text: "Hippo", points: 5),
-        Answer(text: "Chiken", points: 4),
-        Answer(text: "Human", points: 1)
-     ]
-)
-
-var game2 = Round(
-    question: "What is your favourite animal 2?",
-     answers: [
-        Answer(text: "Dog", points: 40),
-        Answer(text: "Cat", points: 30),
-        Answer(text: "Turtle", points: 10),
-        Answer(text: "Hippo", points: 5),
-        Answer(text: "Chiken", points: 4),
-        Answer(text: "Human", points: 1)
-     ]
-)
-
-var game3 = Round(
-    question: "What is your favourite animal 3?",
-     answers: [
-        Answer(text: "Dog", points: 40),
-        Answer(text: "Cat", points: 30),
-        Answer(text: "Turtle", points: 10),
-        Answer(text: "Hippo", points: 5),
-        Answer(text: "Chiken", points: 4),
-        Answer(text: "Human", points: 1)
-     ]
-)
-
-var games = [game1, game2, game3]
---
+++ FamilyFeud/Types.swift
@@ -0,0 +1,177 @@
+import SwiftUI
+
+struct GameState {
+    var player1: Bool
+    var pointsP1: Int = 0
+    var pointsP2: Int = 0
+    var currentPoints: Int = 0
+    var teamName1: String
+    var teamName2: String
+    var roundCount: Int = 0
+    var round: Round = games.randomElement().unsafelyUnwrapped
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Text(round.question)
+            Spacer()
+          
+            List(round.answers.indices, id: \.self) { i in
+                HStack {
+                    Text(round.answers[i].getText())
+                    Spacer()
+                    Text(round.answers[i].getPoints())
+                }
+            }.lineLimit(8)
+        }
+    }
+    
+    var scoreTable: some View {
+        HStack {
+            Spacer()
+            VStack {
+                Text(teamName1)
+                Text(String(pointsP1))
+            }
+            Spacer()
+            
+            Spacer()
+            VStack {
+                Text(teamName2)
+                Text(String(pointsP2))
+            }
+            Spacer()
+        }
+    }
+    
+    var currentTeam: String {
+        player1 ? teamName1 : teamName2
+    }
+    
+    mutating func addPoints(_ points: Int) -> Void {
+        if(player1) { pointsP1 += points }
+        else { pointsP2 += points }
+    }
+    
+    func nextRound() -> GameState {
+        if (player1) {
+            return GameState(player1: false, pointsP1: pointsP1 + currentPoints, pointsP2: pointsP2, teamName1: teamName1, teamName2: teamName2, roundCount: roundCount + 1)
+        } else {
+            return GameState(player1: true, pointsP1: pointsP1, pointsP2: pointsP2 + currentPoints, teamName1: teamName1, teamName2: teamName2, roundCount: roundCount + 1)
+        }
+    }
+    
+    func copy(player1: Bool? = nil, currentPoints: Int? = nil) -> GameState {
+        return GameState(
+            player1: player1 ?? self.player1,
+            pointsP1: self.pointsP1,
+            pointsP2: self.pointsP2,
+            currentPoints: currentPoints ?? self.currentPoints,
+            teamName1: self.teamName1,
+            teamName2: self.teamName2,
+            roundCount: self.roundCount + 1,
+            round: self.round)
+    }
+}
+
+struct Round {
+    var question: String
+    var answers: [Answer]
+    var strikes: Int = 0
+    
+    mutating func tryAnswer(_ ans: String) -> Int {
+        for i in 0...answers.count - 1 {
+            if (answers[i].isCorrectAnswer(ans)) {
+                answers[i].isGuessed = true
+                return answers[i].points
+            }
+        }
+        return 0
+    }
+    
+    func maxPoints() -> Int {
+        let sorted: [Answer] = answers.sorted(by: { $0.points > $1.points})
+        return sorted[0].points
+    }
+    
+    func isFinished() -> Bool {
+        for i in 0...answers.count - 1 {
+            if (!answers[i].isGuessed) {
+                return false
+            }
+        }
+        return true
+    }
+    
+    mutating func reveal() -> Void {
+        for i in 0...answers.count - 1 {
+            answers[i].isGuessed = true
+        }
+    }
+}
+
+struct Answer {
+    var text: String
+    var points: Int
+    var isGuessed: Bool = false
+    
+    func isCorrectAnswer(_ ans: String) -> Bool {
+        ans.lowercased() == text.lowercased()
+    }
+    
+    func getText() -> String {
+        if (isGuessed) {
+            return text
+            
+        } else {
+            return ""
+        }
+    }
+    
+    func getPoints() -> String {
+        if (isGuessed) {
+            return "\(points)"
+            
+        } else {
+            return ""
+        }
+    }
+}
+
+var game1 = Round(
+    question: "What is your favourite animal?",
+     answers: [
+        Answer(text: "Dog", points: 40),
+        Answer(text: "Cat", points: 30),
+        Answer(text: "Turtle", points: 10),
+        Answer(text: "Hippo", points: 5),
+        Answer(text: "Chiken", points: 4),
+        Answer(text: "Human", points: 1)
+     ]
+)
+
+var game2 = Round(
+    question: "What is your favourite animal 2?",
+     answers: [
+        Answer(text: "Dog", points: 40),
+        Answer(text: "Cat", points: 30),
+        Answer(text: "Turtle", points: 10),
+        Answer(text: "Hippo", points: 5),
+        Answer(text: "Chiken", points: 4),
+        Answer(text: "Human", points: 1)
+     ]
+)
+
+var game3 = Round(
+    question: "What is your favourite animal 3?",
+     answers: [
+        Answer(text: "Dog", points: 40),
+        Answer(text: "Cat", points: 30),
+        Answer(text: "Turtle", points: 10),
+        Answer(text: "Hippo", points: 5),
+        Answer(text: "Chiken", points: 4),
+        Answer(text: "Human", points: 1)
+     ]
+)
+
+var games = [game1, game2, game3]
