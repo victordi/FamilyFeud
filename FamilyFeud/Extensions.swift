8078b2125f99f3582053c03104f7347f5b6ccc50
Victor <victor.diaconu@ing.com>
2022-06-06 09:33:31 +0300

    Small fix for whitescreen

--- FamilyFeud/Extensions.swift
+++ FamilyFeud/Extensions.swift
@@ -8,13 +8,13 @@ extension View {
     func navigate<NewView: View>(to view: NewView, when binding: Binding<Bool>) -> some View {
         NavigationView {
             ZStack {
+                let screen = UIScreen.main.bounds
                 self
-                    .navigationBarTitle("")
                     .navigationBarHidden(true)
+                    .frame(width: screen.width, height: screen.height, alignment: Alignment.center)
 
                 NavigationLink(
                     destination: view
-                        .navigationBarTitle("")
                         .navigationBarHidden(true),
                     isActive: binding
                 ) {
